9b7ac6b09f0056b2f2f96456e44854f83b1b0743
Dominik Vach <vach.dominik@gmail.com>
2022-01-13 00:00:27 +0100

    file queue initialization

--- filehydra/filehydra_core.py
+++ filehydra/filehydra_core.py
@@ -75,4 +75,62 @@ class FileTemplate:
         elif self.type=="xlsx":
             df=pd.read_excel(filename,index_col=0)
             return(df)
+
+    
+class FileQueue:
+    def __init__(self,root_path,name,filehydra=None,filename_order_lambda_function=lambda x:x):
+        self.root_path=root_path
+        self.name=name
+        self.queue_path=self.root_path+"\\"+self.name
+        
+        if filehydra is None:    
+            self.filehydra=FileHydra(self.root_path)
+        else:
+            self.filehydra=filehydra
+            
+        self.filename_order_lambda_function=filename_order_lambda_function #e.g. lambda x:int(x.split("_")[0]) - input filename, output ordered unique number
+        
+        self.files_in_queue=[]
+        
+    def initialize_queue_folders(self):
+        self.filehydra.create_folder(self.name)
+        print(self.filehydra.location)
+        self.filehydra.change_hydra_location(self.root_path+"\\"+self.name)
+        print(self.filehydra.location)
+        self.filehydra.create_folder("processed")
+        
+    def refresh_files_in_queue(self,file_template):
+        
+        files=get_files_in_directory(self.queue_path,file_template.suffix)
+        self.files_in_queue=name_contains(file_template.prefix,files)
+        
+        order_to_file_dict={self.filename_order_lambda_function(x):x for x in self.files_in_queue}
+        ordered_files_in_queue=sorted([self.filename_order_lambda_function(x) for x in self.files_in_queue])
+        self.files_in_queue=[order_to_file_dict[x] for x in ordered_files_in_queue]
+        print(ordered_files_in_queue)
+        
+    
+    def process_next_file(self,file_template):
+       
+        self.refresh_files_in_queue(file_template)
+        
+        try:
+            filename=self.files_in_queue[0]
+            print(filename)
+            data=file_template.extract_data(filename)
+            
+            self.filehydra.move_file(filename,self.queue_path, self.queue_path+"\\processed")
+            
+            return(filename,data)
+        except IndexError:
+            print("No suitable file - no move")
+            return("",None)
+            
+        
+        
+        
+        
+        
+        
+        
         
